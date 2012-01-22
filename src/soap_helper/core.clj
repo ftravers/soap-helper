8eced83075eed070679f1ec1cc4568dca2e77791
Fenton Travers <fenton.travers@oracle.com>
2012-01-22 16:43:40 +0800

    Initial Commit

---
+++ src/soap_helper/core.clj
@@ -0,0 +1,1 @@
+(ns soap-helper.core)
---
+++ test/soap_helper/test/core.clj
@@ -0,0 +1,6 @@
+(ns soap-helper.test.core
+  (:use [soap-helper.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

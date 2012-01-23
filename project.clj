dcb8af246861be9dd9c89a8532edf44c905c5275
Fenton Travers <fenton.travers@oracle.com>
2012-01-23 10:56:30 +0800

    .

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
-(defproject soap-helper "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject com.oracle.git.ngsp/SoapHelper "1.0.0-SNAPSHOT"
+  :description "Help process SOAP messages"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[ swank-clojure "1.2.1"]
+                     [com.stuartsierra/lazytest "1.1.2"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
+  :main com.oracle.git.ngsp.SoapHelper)
+
--- test/soap_helper/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns soap-helper.test.core
-  (:use [soap-helper.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
